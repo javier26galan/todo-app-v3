c8602f1602ac10f785be90298df7d75aeb690065
Francisco Javier Galán Guerrero <galan.guererro@bootcamp-upgrade.com>
2023-06-30 17:49:49 +0200

    todo.model.ts modified

--- backend/models/todo.model.js
+++ backend/models/todo.model.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const User = require("./user.model");
 
 
 const todoSchema = mongoose.Schema({
   title: { type: String, required: true },
-  content: { type: String, required: true }
+  content: { type: String, required: true },
+  user: {type: Schema.ObjectId, ref:"User"}
 });
 
 module.exports = mongoose.model("User", todoSchema);
